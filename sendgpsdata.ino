0c9a2d876be3826e90946611ac402c5bade1ee0a
Furkanbayra <furkanbayrak14@gmail.com>
2023-07-28 15:20:12 +0300

    raw data and processed data

--- sendgpsdata.ino
+++ sendgpsdata.ino
@@ -11,7 +11,13 @@ volatile int seconds = 0;
 String rmcData;
 String ggaData;
 String gllData;
+String vtgData;
 String finally;
+String finallyRaw;
+String RAWrmcData;
+String RAWggaData;
+String RAWgllData;
+String RAWvtgData;
 void setup() {
   while (!Serial)
     ;
@@ -32,17 +38,14 @@ void setup() {
 
 void loop() {
   if (minuteElapsed) {
+    printfinalString();
     minuteElapsed = false;
-
-    if (finally.length() > 0 ) {
-      Serial.println(finally);
-      rmcData = "";
-      ggaData="";
-      finally="";
-    }
-    
   }
+  readData();
+  createFinalString();
+}
 
+void readData() {
   if (NMEA.available()) {
     char receivedChar = NMEA.read();
 
@@ -70,31 +73,52 @@ void loop() {
         String EW = fields[6].c_str();
         String SOG = fields[7].c_str();
         String COG = fields[8].c_str();
-        rmcData = latitude + " " + NS + " " + longitude + " " + EW+" "+SOG + " " +COG;
-      }
-      else if(fields[0]=="$GPGGA"){
+        rmcData = latitude + " " + NS + " " + longitude + " " + EW + " " + SOG + " " + COG;
+        RAWrmcData = "";
+        for (int i = 0; i <= 11; i++) {
+          RAWrmcData += fields[i].c_str();
+          if (i < 11) {
+            RAWrmcData += ",";
+          }
+        }
+      } else if (fields[0] == "$GPGGA") {
         String latitude = fields[2].c_str();
         String NS = fields[3].c_str();
         String longitude = fields[4].c_str();
         String EW = fields[5].c_str();
-        ggaData= latitude+ " "+ NS + " "+ longitude+ " "+ EW;
-      }
-      else if (fields[0]=="$GPGLL"){
+        ggaData = latitude + " " + NS + " " + longitude + " " + EW;
+        RAWggaData = "";
+        for (int i = 0; i <= 11; i++) {
+          RAWggaData += fields[i].c_str();
+          if (i < 11) {
+            RAWggaData += ",";
+          }
+        }
+      } else if (fields[0] == "$GPGLL") {
         String latitude1 = fields[3].c_str();
         String NS1 = fields[2].c_str();
         String longitude1 = fields[5].c_str();
         String EW1 = fields[4].c_str();
-        gllData =  latitude1+ " "+ NS1 + " "+ longitude1+ " "+ EW1;
+        gllData = latitude1 + " " + NS1 + " " + longitude1 + " " + EW1;
+        RAWgllData = "";
+        for (int i = 0; i <= 6; i++) {
+          RAWgllData += fields[i].c_str();
+          if (i < 6) {
+            RAWgllData += ",";
+          }
+        }
+      } else if (fields[0] == "$GPVTG") {
+        String course = fields[1].c_str();
+        String SOG = fields[5].c_str();
+        vtgData = course + " " + SOG;
+        RAWvtgData = "";
+        for (int i = 0; i <= 8; i++) {
+          RAWvtgData += fields[i].c_str();
+          if (i < 8) {
+            RAWvtgData += ",";
+          }
+        }
       }
-
-      if (rmcData.length()>0){
-        ggaData="";
-        gllData="";
-      }
-      else if(ggaData.length()>0){
-        gllData="";
-      }
-      finally = rmcData+" "+ ggaData+" "+gllData;
     } else {
       buffer[bufferIndex] = receivedChar;
       bufferIndex++;
@@ -104,6 +128,37 @@ void loop() {
     }
   }
 }
+
+void createFinalString() {
+  if (rmcData.length() > 0) {
+    finally = rmcData;
+    finallyRaw = RAWrmcData;
+  } else if (ggaData.length() > 0) {
+    finally = ggaData;
+    finallyRaw = RAWggaData;
+  } else if (gllData.length() > 0) {
+    finally = gllData;
+    finallyRaw = RAWgllData;
+  } else if (vtgData.length() > 0) {
+    finally = vtgData;
+    finallyRaw = RAWvtgData;
+  } else {
+    finally = "NO CONNECTION";
+  }
+}
+void printfinalString() {
+  if (finally.length() > 0) {
+    Serial.println(finally);    
+    finally = "";
+  }
+}
+void printfinalRawString() {
+  if (finallyRaw.length() > 0) {
+    Serial.println(finallyRaw);
+    finallyRaw = "";
+  }
+}
+
 ISR(TIMER1_COMPA_vect) {
   seconds++;
   if (seconds >= 10) {  // time adjust in second
